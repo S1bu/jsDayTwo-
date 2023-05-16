a456bb3069fa508fbcf1c5ca7e326ac94f51b74a
S1bu <Sibusisodyan@gmail.com>
2023-05-16 14:41:01 +0200

    project-done

---
+++ script/code.js
@@ -0,0 +1,8 @@
+let subject =[ 'Python','C++', 'VB.Net','Javascript','Assembly']
+
+// console.log(subject[4]);//last element
+// console.log(subject[2]);//middle element
+console.log(subject[subject. length - 1] ) //last number
+
+let middle = subject[Math.round((subject.length - 1) / 2)];
+console.log(middle); //middle element
